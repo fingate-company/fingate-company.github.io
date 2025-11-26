6b22dfda82cd49078e890c9c5d034dab6612d246
박효빈 <leenstar@naver.com>
2025-11-26 11:48:21 +0900

    수정

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,20 +22,9 @@ const appRef = ref<HTMLElement | null>(null)
 const updateScale = () => {
   if (typeof window === 'undefined' || !appRef.value) return
   
-  const viewportWidth = 1920 // 고정 viewport 너비
-  const screenWidth = window.innerWidth
-  
-  if (screenWidth < viewportWidth) {
-    const scale = screenWidth / viewportWidth
-    appRef.value.style.transform = `scale(${scale})`
-    appRef.value.style.transformOrigin = 'top left'
-    appRef.value.style.width = `${viewportWidth}px`
-    appRef.value.style.height = `${window.innerHeight / scale}px`
-  } else {
-    appRef.value.style.transform = 'none'
-    appRef.value.style.width = '100%'
-    appRef.value.style.height = 'auto'
-  }
+  // viewport meta tag가 스케일링을 처리하므로 별도 처리 불필요
+  appRef.value.style.width = '100%'
+  appRef.value.style.height = 'auto'
 }
 
 onMounted(() => {
